29d606bf17104395412492e00bbf7980d1235847
tiffie27 <lim.steffie@gmail.com>
2014-12-07 14:07:01 +0100

    71214

--- test3.c
+++ test3.c
@@ -43,101 +43,122 @@ void test3(const char *fic, char *type){
 	if (fichier_ouvert != NULL){
         printf("Le fichier \"%s\" à bien été ouvert\n", fic); //confirme que le fichier est ouvert et qu'on peut travailler dessus
 
-        	while(fgets(ligne_lue, BUFFER, fichier_ouvert) != NULL){ //boucle qui recupere le nombre de lignes du fichier
-                //printf("%s\n",ligne_lue );
-        		nb_lignes++;
-        	}
+    	while(fgets(ligne_lue, BUFFER, fichier_ouvert) != NULL){ //boucle qui recupere le nombre de lignes du fichier
+            //printf("%s\n",ligne_lue );
+    		nb_lignes++;
+    	}
 
-           // fseek(fichier_ouvert,0,SEEK_SET); //remet au debut du fichier le pointeur
-            rewind(fichier_ouvert); //remet au debut du fichier le pointeur
+       // fseek(fichier_ouvert,0,SEEK_SET); //remet au debut du fichier le pointeur
+        rewind(fichier_ouvert); //remet au debut du fichier le pointeur
 
 
-        	while(i<nb_lignes){
+    	while(i<nb_lignes){
 
-                if(*type == 'c' || *type == 'C'){
+            if(*type == 'c' || *type == 'C'){
 
-                    acteur = (Personne*) malloc(nb_lignes * sizeof(Personne)); //alloue la memoire du tableau par rapport au nombre de lignes lues
+                acteur = (Personne*) malloc(nb_lignes * sizeof(Personne)); //alloue la memoire du tableau par rapport au nombre de lignes lues
 
-                    fscanf(fichier_ouvert, "%s ,%s ,%d ,%d ,%d ,%s ", acteur[i].prenom, acteur[i].nom, &acteur[i].dob.jour, &acteur[i].dob.mois, &acteur[i].dob.annee, acteur[i].nationalite);
+                fscanf(fichier_ouvert, "%s ,%s ,%d ,%d ,%d ,%s ", acteur[i].prenom, acteur[i].nom, &acteur[i].dob.jour, &acteur[i].dob.mois, &acteur[i].dob.annee, acteur[i].nationalite);
 
-                    acteur[i].statut = 1;
-            
-                    affichagePersonne(&acteur[i]);
+                acteur[i].statut = 1;
+        
+                affichagePersonne(&acteur[i]);
 
-                    i++;
-                }
-
-                if(*type == 'b' || *type == 'B'){
-                    
-                    realisateur = (Personne*) malloc(nb_lignes * sizeof(Personne)); //alloue la memoire du tableau par rapport au nombre de lignes lues
-
-                    fscanf(fichier_ouvert, "%s ,%s ,%d ,%d ,%d ,%s ", realisateur[i].prenom, realisateur[i].nom, &realisateur[i].dob.jour, &realisateur[i].dob.mois, &realisateur[i].dob.annee, realisateur[i].nationalite);
-
-                    realisateur[i].statut = 0;
-            
-                    affichagePersonne(&realisateur[i]);
-
-                    i++;
-                }
-
-                if(*type == 'a' || *type == 'A'){
-                    
-                    film = (Film*) malloc(nb_lignes * sizeof(Film)); //alloue la memoire du tableau par rapport au nombre de lignes lues
-
-                    fscanf(fichier_ouvert, "%d :%d :%d :", &titre, &genre, &nb_act);
-                    
-                    if (titre == 1){
-                        printf("dkdjg\n");
-                        if (genre == 1){
-                             if(nb_act == 2){
-                                fscanf(fichier_ouvert,"%s :%d :%s %s :%d :%s :", film[i].titre, &film[i].annee, film[i].realisateur.prenom, film[i].realisateur.nom, &film[i].duree, g1);
-                                while(j<nb_act){
-                                    fscanf(fichier_ouvert, "%s %s ", film[i].casting[j].prenom, film[i].casting[j].nom);
-                                    j++;
-                                }
-
-                                conv_g1 = conversionChartoEnum(g1);
-                                conv_g2 = conversionChartoEnum(g2);
-
-                                film[i].genre[0] = conv_g1;
-                                film[i].genre[1] = conv_g2;
-                                affichageFilm(&film[i]);
-                            }
-                        }
-                        if (genre == 2){
-                            printf("dfkjjg\n");
-                             if(nb_act == 3){
-
-                                fscanf(fichier_ouvert,"%s :%d :%s %s :%d :%s :%s :", film[i].titre, &film[i].annee, film[i].realisateur.prenom, film[i].realisateur.nom, &film[i].duree, g1, g2);
-                                while(j<nb_act){
-                                    fscanf(fichier_ouvert, "%s %s :", film[i].casting[j].prenom, film[i].casting[j].nom);
-                                    j++;
-                                }
-
-                                conv_g1 = conversionChartoEnum(g1);
-                                conv_g2 = conversionChartoEnum(g2);
-
-                                film[i].genre[0] = conv_g1;
-                                film[i].genre[1] = conv_g2;
-                                 printf("conv %d\n", conv_g1 );
-
-                                affichageFilm(&film[i]);
-                            }
-                        }
-                    }
-                    printf("%d %d %d\n", titre, genre, nb_act);
-                   // affichageFilm(&film[i]);
-
-                    i++;
-                }
-
+                i++;
             }
 
+            if(*type == 'b' || *type == 'B'){
+                
+                realisateur = (Personne*) malloc(nb_lignes * sizeof(Personne)); //alloue la memoire du tableau par rapport au nombre de lignes lues
+
+                fscanf(fichier_ouvert, "%s ,%s ,%d ,%d ,%d ,%s ", realisateur[i].prenom, realisateur[i].nom, &realisateur[i].dob.jour, &realisateur[i].dob.mois, &realisateur[i].dob.annee, realisateur[i].nationalite);
+
+                realisateur[i].statut = 0;
+        
+                affichagePersonne(&realisateur[i]);
+
+                i++;
+            }
+
+            if(*type == 'a' || *type == 'A'){
+                
+                film = (Film*) malloc(nb_lignes * sizeof(Film)); //alloue la memoire du tableau par rapport au nombre de lignes lues
+
+                fscanf(fichier_ouvert, "%d :%d :%d :", &titre, &genre, &nb_act);
+                if (titre == 1){
+                    fscanf(fichier_ouvert,"%s :", film[i].titre);
+                }
+                if (titre == 2){
+                    fscanf(fichier_ouvert,"%s %s :", t1, t2);
+
+                    strcat(t1," ");
+
+                    strcpy(film[i].titre, strcat(t1, t2));
+
+                }  
+
+                if (titre == 3){
+                    fscanf(fichier_ouvert,"%s %s %s :", t1, t2, t3);
+
+                    strcat(t1," ");
+                    strcat(t1, t2);
+                    strcat(t1," ");
+                    strcpy(film[i].titre, strcat(t1, t3));
+                }
+
+                fscanf(fichier_ouvert,"%d :%s %s :%d :", &film[i].annee, film[i].realisateur.prenom, film[i].realisateur.nom, &film[i].duree);
+
+                if (genre == 1){
+                    fscanf(fichier_ouvert,"%s :", g1);
+                    conv_g1 = conversionChartoEnum(g1);
+                    film[i].genre[0] = conv_g1;
+                }
+                
+                if (genre == 2){
+                    fscanf(fichier_ouvert,"%s :%s :", g1, g2);
+                    conv_g1 = conversionChartoEnum(g1);
+                    conv_g2 = conversionChartoEnum(g2);
+
+                    film[i].genre[0] = conv_g1;
+                    film[i].genre[1] = conv_g2;
+                }       
+                        
+                if(nb_act == 1){
+                    fscanf(fichier_ouvert, "%s %s :", film[i].casting[j].prenom, film[i].casting[j].nom);
+                }
+
+                j=0;                                      
+                if(nb_act == 2){
+                    while(j<nb_act){
+                        printf("fdg\n");
+                        fscanf(fichier_ouvert, "%s %s :", film[i].casting[j].prenom, film[i].casting[j].nom);
+                        j++;
+                    }
+                }
+                           
+                    
+                j=0;
+                if(nb_act == 3){
+
+                    while(j<nb_act){
+                        fscanf(fichier_ouvert, "%s %s :", film[i].casting[j].prenom, film[i].casting[j].nom);
+                        j++;
+                    }
+                }
+        
+
+
+                
+
+                affichageFilm(&film[i]);
+            }
+                  
+            i++;
+        }
 
         printf("Il y a %d lignes\n",nb_lignes );
         
      	fclose(fichier_ouvert);
-
     }
     else{
         printf("Impossible d'ouvrir le fichier de configuration\n");
@@ -156,7 +177,7 @@ Genre conversionChartoEnum(const char *string_genre){
     static struct{
         const char *chaine;
         Genre g;
-    }hashmap[] = {{"ACTION", ACTION},{"HORREUR", HORREUR},{"COMEDIE",COMEDIE},{"DOCUMENTAIRE",DOCUMENTAIRE},{"POLICIER",POLICIER}, {"DRAME",DRAME}, {"ANIMATION", ANIMATION}, {"SCIENCE_FICTION", SCIENCE_FICTION}};
+    }hashmap[] = {{"ACTION", ACTION},{"HORREUR", HORREUR},{"COMEDIE",COMEDIE},{"FANTASTIQUE",FANTASTIQUE},{"POLICIER",POLICIER}, {"DRAME",DRAME}, {"ANIMATION", ANIMATION}, {"SCIENCE_FICTION", SCIENCE_FICTION}};
 
     while(strcmp(string_genre, hashmap[i].chaine) != 0){
         i++;
