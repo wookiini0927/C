9a7d56ee49b82f61734c93c4a6c2154f9185ec7b
tiffie27 <lim.steffie@gmail.com>
2014-12-16 02:18:55 +0100

    saisie manuel complet

--- test2.c
+++ test2.c
@@ -1,58 +1,106 @@
 #include "test2.h"
 /*rentrer par l'utilisateur*/
 
-void test2(){
-	int taille_utile_film=0;
-	int taille_utile_real=0;
-	int taille_utile_acteur=0;
+void test2(Film film[], Personne real[], Personne acteur[]){
 
-	char *choix;
-	int numero;
-	char rep;
+	char choix[REPONSE];
+	char rep[REPONSE];
+	Film tmp;
+	Personne real_tmp;
+	Personne act_tmp;
 
-	Film film[TAILLE_DEFAUT];
-	Personne realisateur[TAILLE_DEFAUT];
-	Personne acteur[TAILLE_DEFAUT];
+	int i = 0; //compteur pour le tableau film.casting;
+	/*int j = 0; //compteur pour le tableau real;
+	int k = 0; //compteur pour le tableau act;*/
 
-	/*do{
+	int arret = 1; /* continu la saisie
+					* 0 arrete la saisie
+					*/
+
+	int taille_utile_test2_film = 11; //pour savoir ou en est la taille du tableau film
+    int taille_utile_test2_real = 17; //pour savoir ou en est la taille du tableau real
+    int taille_utile_test2_acteur = 36; // pour savoir ou en est la taille du tableau act
+
+
+	do{
 		do{
 			do{
-				printf("-------------Saisie--------------\n|\t1-Film\t\t\t|\n|\t2-Realisateur\t\t|\n|\t3-Acteur/Actrice\t|\n");
+				printf("-------------Saisie--------------\n|\tA-Film\t\t\t|\n|\tB-Realisateur\t\t|\n|\tC-Acteur/Actrice\t|\n");
 				printf("---------------------------------\n");
 
-				fgets(choix, TAILLE_DEFAUT, stdin);
-			}while(verification_digit(choix) == 0);
+				fgets(choix, REPONSE, stdin);
+			}while(*choix !='A' && *choix !='a' && *choix !='B' && *choix !='b' && *choix != 'C' && *choix != 'c');
 
+			if (*choix == 'A' || *choix == 'a'){
+				clean(choix);
+				saisirFilm(film, real, acteur, taille_utile_test2_film, taille_utile_test2_real, taille_utile_test2_acteur, &tmp);
+				init_tableau_Personne(&real[taille_utile_test2_real], film[taille_utile_test2_film].realisateur.nom, film[taille_utile_test2_film].realisateur.prenom, film[taille_utile_test2_film].realisateur.dob.jour, film[taille_utile_test2_film].realisateur.dob.mois, film[taille_utile_test2_film].realisateur.dob.annee, film[taille_utile_test2_film].realisateur.nationalite, film[taille_utile_test2_film].realisateur.statut);
 
+				//affichageFilm(&film[taille_utile_test2_film]);
+				taille_utile_test2_film++;
+				taille_utile_test2_real++;
 
-			if (choix == 1){
-				saisirFilm(&film[taille_utile_film]);
-				taille_utile_film++;
 			}
-			else if (choix == 2){
-				saisirPersonne(&realisateur[taille_utile_real]);
-				taille_utile_real++;
+			else if (*choix == 'B' || *choix == 'b'){
+				clean(choix);
+				saisirPersonne(real, taille_utile_test2_real, &real_tmp);
+				taille_utile_test2_real++;
 			}
-			else if (choix == 3){
-				saisirPersonne(&acteur[taille_utile_acteur]);
-				taille_utile_acteur++;
+			else if (*choix == 'C' || *choix == 'c'){
+				clean(choix);
+				saisirPersonne(acteur, taille_utile_test2_acteur, &act_tmp);
+				taille_utile_test2_acteur++;
 			}
+			clean(choix);
 
+			printf("Voulez-vous saisir une nouvelle entree? (o/n)\n");
+			fgets(rep, REPONSE, stdin);
 
+			if(rep[0] == 'o' || rep[0] == 'O'){
+				echange_chariot_espace(rep);
 
-		printf("Voulez-vous saisir une nouvelle entree? (o/n)\n");
-		scanf(" %c", &rep);
-
-	}while(rep == 'o' && rep == 'O' && rep =='n' && rep =='N');*/
+				arret = 1;
+				clean(rep);
+			}
+			else if (rep[0] == 'n' || rep[0] == 'N'){
+				echange_chariot_espace(rep);
+				arret = 0;
+				test2extern(film, real,acteur, taille_utile_test2_film,taille_utile_test2_real,taille_utile_test2_acteur);
+				clean(rep);
+			}
+		
+		}while(*rep == 'o' && *rep == 'O' && *rep =='n' && *rep =='N');
+	}while(arret==1);
 
 	/*saisirFilm(&film[0]);
 	affichageFilm(&film[0]);*/
+
 }
 
-/*int main(int argc, char const *argv[])
-{
-	test2();
+void test2extern(Film film[], Personne real[], Personne acteur[], int taille_film, int taille_r, int taille_act){
+	nettoie();
+    char choix[REPONSE];
+    clean(choix);
 
-	return 0;
-}*/
+    do{
+        printf("Une nouvelle base de donnees a ete définie, voulez vous faire une recherche ?\n");
+        printf("N-Non, fin du programme\n");
+        printf("O-Oui, faire une recherche sur la base de donnees\n");
+
+        fgets(choix,REPONSE,stdin);
+
+    }while(*choix !='O' && *choix !='o' && *choix !='N' && *choix !='n');
+
+    if (*choix == 'O' || *choix == 'o'){
+       // stop = 1;
+        clean(choix);
+        test1_menu(film, real, acteur, taille_film, taille_r, taille_act);
+    }
+    else if (*choix == 'N' || *choix == 'n'){
+       // stop = 0;
+        clean(choix);
+        exit(0);
+    }
+
+}
 
--- test2.h
+++ test2.h
@@ -2,12 +2,16 @@
 #define TEST2_H_INCLUDED 
 
 #include <stdio.h>
+#include <stdlib.h>
 #include "film.h"
 #include "date.h"
 #include "personne.h"
+#include "test1.h"
 
 #define TAILLE_DEFAUT 100
 
-void test2();
+void test2(Film tab[], Personne real[], Personne acteur[]);
+
+void test2extern(Film tab[], Personne real[], Personne acteur[], int taille_film, int taille_r, int taille_act);
 
 #endif
